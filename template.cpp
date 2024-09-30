9f970dfa1095088106ddaa0ab8000da240069752
KomuraDaWeeb <161989345+KomuraDaWeeb@users.noreply.github.com>
2024-09-30 15:51:48 +0700

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,19 @@
+//author: kaoruko
+#include<bits/stdc++.h>
+#define int long long
+#define vi vector <int>
+#define lb lower_bound
+#define ub upper_bound
+#define pb push_back
+#define eb emplace_back
+#define fu(i, a, b) for(int i = (a); i < (b); i++)
+#define fub(i, a, b) for(int i = (a); i <= (b); i++)
+#define fd(i, a, b) for(int i = (a); i > (b); i--)
+#define fdb(i, a, b) for(int i = (a); i >= (b); i--)
+#define pii pair<int, int>
+#define PI acos(-1)
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0);
+using namespace std;
+const int lim = 1e18;
+const int N = 1e6 + 5;
+const int M = 1e9 + 7;
