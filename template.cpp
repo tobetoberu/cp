42c44ef9bbd21b096f1bda80410ffd1dcb490ee7
tobetoberu <161989345+tobetoberu@users.noreply.github.com>
2025-05-03 23:02:58 +0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,4 +1,4 @@
-//author: kaoruko
+//author: toberu
 #include<bits/stdc++.h>
 #define int long long
 #define vi vector <int>
@@ -6,14 +6,26 @@
 #define ub upper_bound
 #define pb push_back
 #define eb emplace_back
+#define fi first
+#define se second
 #define fu(i, a, b) for(int i = (a); i < (b); i++)
 #define fub(i, a, b) for(int i = (a); i <= (b); i++)
 #define fd(i, a, b) for(int i = (a); i > (b); i--)
 #define fdb(i, a, b) for(int i = (a); i >= (b); i--)
 #define pii pair<int, int>
 #define PI acos(-1)
+#define EPS 1e-8
+#define inv(x) (x & (-x))
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0);
 using namespace std;
 const int lim = 1e18;
 const int N = 1e6 + 5;
 const int M = 1e9 + 7;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rand(int l, int r) {
+    assert(l <= r);
+    return uniform_int_distribution<int> (l, r)(rng);
+}
+signed main(){
+    fastio;
+}
